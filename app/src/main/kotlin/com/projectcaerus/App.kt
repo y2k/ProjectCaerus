5513e2180943af120e3dcf5afad4176309981135
y2k <itwisterlx@gmail.com>
2016-06-04 19:29:37 +0400

    gradle project

---
+++ app/src/main/kotlin/com/projectcaerus/App.kt
@@ -0,0 +1,9 @@
+package com.projectcaerus
+
+//
+// Created by y2k on 6/4/2016.
+//
+
+fun main(args:Array<String>) {
+    println("test")
+}
---
+++ app/src/test/kotlin/com/projectcaerus/ButtonTest.kt
@@ -0,0 +1,19 @@
+package com.projectcaerus
+
+import android.widget.Button
+import org.junit.Test
+
+//
+// Created by y2k on 6/4/2016.
+//
+
+class ButtonTest {
+
+    @Test
+    fun test() {
+        // TODO:
+
+        Button(null)
+
+    }
+}
