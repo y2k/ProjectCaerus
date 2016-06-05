2e7ef086948c1b503725e571841cd0a88b97fb6a
y2k <itwisterlx@gmail.com>
2016-06-05 12:00:36 +0400

    test linearlayout

--- app/src/test/kotlin/com/projectcaerus/ButtonTest.kt
+++ app/src/test/kotlin/com/projectcaerus/ButtonTest.kt
@@ -9,7 +9,10 @@ import android.emoji.EmojiFactory
 import android.graphics.Canvas
 import android.util.DisplayMetrics
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.*
@@ -26,11 +29,14 @@ import org.powermock.modules.junit4.PowerMockRunner
 @PrepareForTest(Resources.Theme::class, Context::class, EmojiFactory::class)
 class ButtonTest {
 
-    @Test
-    fun test() {
+    lateinit var context: Context
+    lateinit var canvas: Canvas
+
+    @Before
+    fun setUp() {
         PowerMockito.mockStatic(EmojiFactory::class.java)
 
-        val context = PowerMockito.mock(Context::class.java)
+        context = PowerMockito.mock(Context::class.java)
         when_(context.resources).then {
             val resources = mock(Resources::class.java)
             when_(resources.displayMetrics).then {
@@ -45,14 +51,17 @@ class ButtonTest {
             mock(TypedArray::class.java)
         }
 
-        val canvas = mock(Canvas::class.java, Answer {
+        canvas = mock(Canvas::class.java, Answer {
             println("called = " + it)
 
             if (it.method.name == "save") 1
             else null
         })
         when_(canvas.save()).then { 1 }
+    }
 
+    @Test
+    fun test() {
         val button = Button(context)
         button.text = "Hello World"
 
@@ -62,5 +71,29 @@ class ButtonTest {
         button.layout(0, 0, 200, 200)
 
         button.draw(canvas)
+    }
+
+    @Test
+    fun testLinearLayout() {
+        val linearLayout = LinearLayout(context).apply {
+            orientation = LinearLayout.VERTICAL
+
+            addView(
+                Button(context).apply {
+                    text = "Button 1"
+                    layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1f)
+                })
+            addView(
+                Button(context).apply {
+                    text = "Button 2"
+                    layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1f)
+                })
+        }
+
+        linearLayout.measure(
+            View.MeasureSpec.makeMeasureSpec(200, View.MeasureSpec.EXACTLY),
+            View.MeasureSpec.makeMeasureSpec(200, View.MeasureSpec.EXACTLY))
+        linearLayout.layout(0, 0, 200, 200)
+        linearLayout.draw(canvas)
     }
 }
