449665bd08dfb19915d5257ff6ba7314f00df4c6
y2k <itwisterlx@gmail.com>
2016-06-13 13:44:51 +0400

    layout inflater

--- app/src/test/kotlin/android/content/res/LayoutInflateTest.kt
+++ app/src/test/kotlin/android/content/res/LayoutInflateTest.kt
@@ -2,6 +2,7 @@ package android.content.res
 
 import android.content.Context
 import android.emoji.EmojiFactory
+import android.view.View
 import com.android.internal.policy.impl.PhoneLayoutInflater
 import org.junit.Assert.assertNotNull
 import org.junit.Before
@@ -30,5 +31,11 @@ class LayoutInflateTest {
 
         val view = inflater.inflate(0x01090058, null)
         assertNotNull(view)
+
+        val screenSize = 320 to 480
+        view.measure(
+                View.MeasureSpec.makeMeasureSpec(screenSize.first, View.MeasureSpec.EXACTLY),
+                View.MeasureSpec.makeMeasureSpec(screenSize.second, View.MeasureSpec.EXACTLY))
+        view.layout(0, 0, screenSize.first, screenSize.second)
     }
 }
