11ea2bb051ba661fe461e5df76d3cd6ddcab241f
y2k <itwisterlx@gmail.com>
2016-06-13 14:39:04 +0400

    fixed: sample

--- samples/hello-world/app/src/main/java/com/example/helloworld/MainActivity.java
+++ samples/hello-world/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.helloworld;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
