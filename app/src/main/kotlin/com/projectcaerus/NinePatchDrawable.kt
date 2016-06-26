34f4d978e13bb2effdb210ed9bc211477b61b91e
y2k <itwisterlx@gmail.com>
2016-06-26 20:15:55 +0400

    reformat left code padding

--- app/src/main/kotlin/com/projectcaerus/NinePatchDrawable.kt
+++ app/src/main/kotlin/com/projectcaerus/NinePatchDrawable.kt
@@ -29,17 +29,17 @@ class NinePatchDrawable(path: File) : Drawable() {
         bitmap = Bitmap(image.getSubimage(1, 1, image.width - 2, image.height - 2))
 
         srcRect = listOf(
-                Rect(0, 0, hResize.first, vResize.first),
-                Rect(hResize.first, 0, bitmap.width - hResize.second, vResize.first),
-                Rect(bitmap.width - hResize.second, 0, bitmap.width, vResize.first),
+            Rect(0, 0, hResize.first, vResize.first),
+            Rect(hResize.first, 0, bitmap.width - hResize.second, vResize.first),
+            Rect(bitmap.width - hResize.second, 0, bitmap.width, vResize.first),
 
-                Rect(0, vResize.first, hResize.first, bitmap.height - vResize.second),
-                Rect(hResize.first, vResize.first, bitmap.width - hResize.second, bitmap.height - vResize.second),
-                Rect(bitmap.width - hResize.second, vResize.first, bitmap.width, bitmap.height - vResize.second),
+            Rect(0, vResize.first, hResize.first, bitmap.height - vResize.second),
+            Rect(hResize.first, vResize.first, bitmap.width - hResize.second, bitmap.height - vResize.second),
+            Rect(bitmap.width - hResize.second, vResize.first, bitmap.width, bitmap.height - vResize.second),
 
-                Rect(0, bitmap.height - vResize.second, hResize.first, bitmap.height),
-                Rect(hResize.first, bitmap.height - vResize.second, bitmap.width - hResize.second, bitmap.height),
-                Rect(bitmap.width - hResize.second, bitmap.height - vResize.second, bitmap.width, bitmap.height)
+            Rect(0, bitmap.height - vResize.second, hResize.first, bitmap.height),
+            Rect(hResize.first, bitmap.height - vResize.second, bitmap.width - hResize.second, bitmap.height),
+            Rect(bitmap.width - hResize.second, bitmap.height - vResize.second, bitmap.width, bitmap.height)
         )
     }
 
@@ -56,17 +56,17 @@ class NinePatchDrawable(path: File) : Drawable() {
 
     override fun draw(canvas: Canvas) {
         listOf(
-                Rect(bounds.left, bounds.top, bounds.left + hResize.first, bounds.top + vResize.first),
-                Rect(bounds.left + hResize.first, bounds.top, bounds.right - hResize.second, bounds.top + vResize.first),
-                Rect(bounds.right - hResize.second, bounds.top, bounds.right, bounds.top + vResize.first),
+            Rect(bounds.left, bounds.top, bounds.left + hResize.first, bounds.top + vResize.first),
+            Rect(bounds.left + hResize.first, bounds.top, bounds.right - hResize.second, bounds.top + vResize.first),
+            Rect(bounds.right - hResize.second, bounds.top, bounds.right, bounds.top + vResize.first),
 
-                Rect(bounds.left, bounds.top + vResize.first, hResize.first, bounds.bottom - vResize.second),
-                Rect(bounds.left + hResize.first, bounds.top + vResize.first, bounds.right - hResize.second, bounds.bottom - vResize.second),
-                Rect(bounds.right - hResize.second, bounds.top + vResize.first, bounds.right, bounds.bottom - vResize.second),
+            Rect(bounds.left, bounds.top + vResize.first, hResize.first, bounds.bottom - vResize.second),
+            Rect(bounds.left + hResize.first, bounds.top + vResize.first, bounds.right - hResize.second, bounds.bottom - vResize.second),
+            Rect(bounds.right - hResize.second, bounds.top + vResize.first, bounds.right, bounds.bottom - vResize.second),
 
-                Rect(bounds.left, bounds.bottom - vResize.second, bounds.left + hResize.first, bounds.bottom),
-                Rect(bounds.left + hResize.first, bounds.bottom - vResize.second, bounds.right - hResize.second, bounds.bottom),
-                Rect(bounds.right - hResize.second, bounds.bottom - vResize.second, bounds.right, bounds.bottom)
+            Rect(bounds.left, bounds.bottom - vResize.second, bounds.left + hResize.first, bounds.bottom),
+            Rect(bounds.left + hResize.first, bounds.bottom - vResize.second, bounds.right - hResize.second, bounds.bottom),
+            Rect(bounds.right - hResize.second, bounds.bottom - vResize.second, bounds.right, bounds.bottom)
         ).forEachIndexed { i, rect ->
             canvas.drawBitmap(bitmap, srcRect[i], rect, null)
         }
